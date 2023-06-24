af3f25bf10960cd701dd8575b281c59ccfd69d6a
CR9 <106897958+gebali4802@users.noreply.github.com>
2023-06-24 03:41:24 +0000

    main

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import os
 import util
 
-os.system("title [ 더러운 우리집 ]\033[95m")
+os.system("title [ 더러운 우리집 ]")
 
+result = None
 print("\033[90m[ 더러운 우리집 ]")
-util.info("이 게임은 머드 게임으로 고전 게임처럼 만들었습니다.")
 util.warning("이 게임은 게임 저장이 없으며, 선택지 선택을 틀리지 않도록 조심해주세요.")
-result = util.gameInput([[1, 2, 3], ["게임 시작", "게임 종료", "정보"]])
-if result == None:
-    while result != None:
-        result = util.gameInput([[1, 2, 3], ["게임 시작", "게임 종료", "정보"]])
-else:
-    util.info("정상적작동")
+util.info("이 게임은 머드 게임으로 고전 게임처럼 만들었습니다.")
+result = util.gameInput(["게임 시작", "게임 종료", "정보"])
+while result != None:
+    result = util.gameInput(["게임 시작", "게임 종료", "정보"])
+util.info("정상")
--- util.py
+++ util.py
@@ -1,13 +1,13 @@
 import time
 
 def info(msg):
-    print(f"\033[94m[ 정보 ] : {msg}\033[95m")
+    print(f"\033[94m[ 정보 ] : {msg}\033[90m")
 
 def warning(msg, type="game"):
     if type == "game":
-        print(f"\033[93m[ 알림 ] : {msg}\033[95m")
+        print(f"\033[93m[ 알림 ] : {msg}\033[90m")
     elif "system":
-        print(f"\033[91m[ 경고 ]  : {msg}\033[95m")
+        print(f"\033[91m[ 경고 ]  : {msg}\033[90m")
 
 def gameExit(msg):
     warning(f"게임에 충돌이 나 게임을 강제로 종료합니다!\n이유: {msg}", "system")
@@ -18,9 +18,10 @@ def gameInput(option):
     info("선택지가 나타났습니다!")
     local = 1
     while local <= len(option):
-        print(f"\033[90m[ {local} ] : {option[local - 1]}\033[95m")
+        print(f"\033[92m[ {local} ] : {option[local-1]}\033[90m")
         local += 1
+    result = input("\033[94m[ 선택 ] : \033[90m")
     for i in range(len(option)):
-        if i != result:
+        if i+1 != result:
             warning("선택지를 잘못 고른것 같아요!")
             return None
