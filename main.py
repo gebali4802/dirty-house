8919c896ede0a771f4ccee8db833da9805fb950a
CR9 <106897958+gebali4802@users.noreply.github.com>
2023-06-24 03:04:42 +0000

    main

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
 import util
+
+os.system("title [ 더러운 우리집 ]")
 
 print("\033[90m[ 더러운 우리집 ]")
 util.info("이 게임은 머드 게임으로 고전 게임처럼 만들었습니다.")
-util.input()
+util.warning("이 게임은 게임 저장이 없으며, 선택지 선택을 틀리지 않도록 조심해주세요.")
+result = util.input(["게임 시작", "게임 종료", "정보"])
+
+if result
--- util.py
+++ util.py
@@ -1,15 +1,26 @@
+import time
+
 def info(msg):
     print(f"\033[94m[정보] : {msg}\033[95m")
 
 def warning(msg, type="game"):
     if type == "game":
         print(f"\033[93m[알림] : {msg}\033[95m")
-    else:
+    elif "system":
         print(f"\033[91m[경고]  : {msg}\033[95m")
 
-def inputer(option):
+def gameExit(msg):
+    warning(f"게임에 충돌이 나 게임을 강제로 종료합니다!\n이유: {msg}", "system")
+    time.sleep(3)
+    exit()
+
+def gameInput(option):
     info("선택지가 나타났습니다!")
     for i in range(len(option)):
         for j in range(option):
             print(f"\033[93m[{i}] : {j}\033[95m")
-    return input("\033[93m[선택] : \033[95m")
+    result = input("\033[93m[선택] : \033[95m")
+    for i in range(len(option)):
+        if i != result:
+            warning("선택지를 잘못 고른것 같아요!")
+            return None
